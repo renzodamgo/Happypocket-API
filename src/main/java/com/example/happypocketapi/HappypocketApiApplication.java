df5598faccf134cf19d67f9cc264b24845036d81
Renzo Damian <renzo.damian.go@gmail.com>
2021-05-07 18:13:53 -0500

    Initial commit

---
+++ src/main/java/com/example/happypocketapi/HappypocketApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.happypocketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappypocketApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappypocketApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/happypocketapi/HappypocketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.happypocketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappypocketApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
